776a46d4f966c66da59e315f469da3f37fb767c6
Djevayo Patty <pattijdlr@gmail.com>
2020-09-27 18:29:26 +0200

    args: Args: argv iterator class

---
+++ args/Args.cpp
@@ -0,0 +1,75 @@
+//
+// Created by boat on 9/27/20.
+//
+
+#include "Args.hpp"
+#include "../utils/utils.hpp"
+
+namespace args {
+
+Args::Args(int argc, const char** argv)
+    : _argv(argv)
+    , _argc(argc)
+{
+}
+
+auto Args::begin() const noexcept -> ArgsIterator {
+    return ArgsIterator(_argv, 0);
+}
+
+auto Args::end() const noexcept -> ArgsIterator
+{
+    return ArgsIterator(_argv, _argc);
+}
+
+auto Args::count() const noexcept -> int
+{
+    return _argc;
+}
+
+ArgsIterator::ArgsIterator(const char** argv, int ca)
+    : _argv(argv)
+    , _current_arg(ca)
+{
+}
+
+auto ArgsIterator::operator++() -> ArgsIterator&
+{
+    *this += 1;
+    return *this;
+}
+
+auto ArgsIterator::operator--() -> ArgsIterator&
+{
+    *this -= 1;
+    return *this;
+}
+
+auto ArgsIterator::operator+=(ssize_t n) -> ArgsIterator&
+{
+    _current_arg += n;
+    return *this;
+}
+
+auto ArgsIterator::operator-=(ssize_t n) -> ArgsIterator&
+{
+    _current_arg -= n;
+    return * this;
+}
+
+auto ArgsIterator::operator*() -> string_view
+{
+    return string_view(_argv[_current_arg]);
+}
+
+auto ArgsIterator::operator==(ArgsIterator& other) const -> bool
+{
+    return _current_arg == other._current_arg;
+}
+
+auto ArgsIterator::operator!=(ArgsIterator& other) const -> bool
+{
+    return !(*this == other);
+}
+
+}
