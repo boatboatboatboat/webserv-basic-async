d6fef178bc75cbb806ab1668b0eec1b674e2a81d
Djevayo Patty <pattijdlr@gmail.com>
2020-09-29 18:53:11 +0200

    utils: dbg_puts: changed output from STDOUT to STDERR

--- utils/dbg_puts.cpp
+++ utils/dbg_puts.cpp
@@ -9,9 +9,9 @@ void dbg_puts(std::string const& printme)
 {
     unsigned int len = printme.length();
     const char* str = printme.c_str();
-    int r = write(STDOUT_FILENO, str, len);
+    int r = write(STDERR_FILENO, str, len);
     unsigned int written = r;
     while (r >= 0 && (len - written)) {
-        r = write(STDOUT_FILENO, str + written, len - written);
+        r = write(STDERR_FILENO, str + written, len - written);
     }
 }
