1801a237d15e035d9573c6372a88b3779945b251
Djevayo Patty <pattijdlr@gmail.com>
2020-11-04 22:25:55 +0100

    sfdd

--- net/Socket.cpp
+++ net/Socket.cpp
@@ -53,8 +53,6 @@ auto Socket::write(void const* buffer, size_t size) -> ssize_t
     res = send(descriptor, buffer, size, 0);
 #endif
 #ifdef DEBUG_SOCKET_OUTGOING_PRINTER
-#endif
-#ifndef __AAA__
     if (res > 0) {
         DBGPRINT("SOCKET[" << descriptor << "] " << res << "/" << size << "\n"
                            << std::string_view((const char*)buffer, res));
